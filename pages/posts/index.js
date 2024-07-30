284cd32ed3183d8e56947e7c99e5cdd3e4080aac
unknown <wesman687@gmail.com>
2024-07-30 14:00:30 -0400

    first side done

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,7 +1,23 @@
-export default function Posts(){
-    return (
-        <>
-        <h1>This is the Posts Page</h1>
-        </>
-    )
+import { useEffect, useState } from "react";
+export async function getServerSideProps() {
+    
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await res.json();
+    return {
+        props: {
+            posts: data
+        }
+    }
+}
+export default function Posts({posts}) {
+    
+  return (
+    <>
+      <h1>This is the Posts Page</h1>
+
+      {posts?.map(posts => {
+        return <li key={posts.id}>{posts.title}</li>;
+      })}
+    </>
+  );
 }
